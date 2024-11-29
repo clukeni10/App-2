e97d78b31f9ca3ce40d650c96a7eab446a0c4d33
Aristidescosta <aristidiscosta200@gmail.com>
2024-11-29 16:04:16 +0100

    Design is ready

---
+++ src/UserDataCollection.ts
@@ -0,0 +1,5 @@
+
+const userName = document.getElementById("name") as HTMLInputElement;
+const userEmail = document.getElementById("email") as HTMLInputElement;
+const userNumber = document.getElementById("number") as HTMLInputElement;
+const userImage = document.getElementById("image") as HTMLInputElement;
